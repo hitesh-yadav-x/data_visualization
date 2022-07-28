f55471db7ffafac48eaf2de6c1476543aea70e72
Hitesh Yadav <hitesh.yadav.x@gmail.com>
2022-07-28 00:26:10 -0500

    [HY] added bar charts.

--- script/helper_functions.js
+++ script/helper_functions.js
@@ -1,5 +1,14 @@
 const get_linear_scale = (domain, range) => {
-    //Only set the range and let use set the domain
+    return d3.scaleLinear()
+        .domain(domain)
+        .range(range);
+}
+
+const get_band_scale = (range, padding) => {
+    return d3.scaleBand().rangeRound(range).padding(padding);
+}
+
+const get_log_scale = (domain, range) => {
     return d3.scaleLinear()
         .domain(domain)
         .range(range);
